ebd6a525894595d114eb69237e8dbda60d12243b
Sabya7 <sabya62.ss@gmail.com>
2024-04-02 21:00:07 +0530

    Reactor-POC | Sabya | Day 11 - C11 completed

--- src/test/java/com/sabya/javapoc/reactive/c11_Batching.java
+++ src/test/java/com/sabya/javapoc/reactive/c11_Batching.java
@@ -30,10 +30,7 @@ public class c11_Batching extends BatchingBase {
      */
     @Test
     public void batch_writer() {
-        //todo do your changes here
-        Flux<Void> dataStream = null;
-        dataStream();
-        writeToDisk(null);
+        Flux<Void> dataStream = dataStream().buffer(10).flatMap(this::writeToDisk);
 
         //do not change the code below
         StepVerifier.create(dataStream)
@@ -51,10 +48,7 @@ public class c11_Batching extends BatchingBase {
      */
     @Test
     public void command_gateway() {
-        //todo: implement your changes here
-        Flux<Void> processCommands = null;
-        inputCommandStream();
-        sendCommand(null);
+        Flux<Void> processCommands = inputCommandStream().groupBy(x -> x.getAggregateId()).flatMap(c -> c.concatMap(cmnd -> sendCommand(cmnd)));
 
         //do not change the code below
         Duration duration = StepVerifier.create(processCommands)
@@ -71,7 +65,8 @@ public class c11_Batching extends BatchingBase {
     @Test
     public void sum_over_time() {
         Flux<Long> metrics = metrics()
-                //todo: implement your changes here
+                .window(Duration.ofSeconds(1))
+                .concatMap(x -> x.reduce(0L, (a, b) -> a + b))
                 .take(10);
 
         StepVerifier.create(metrics)
