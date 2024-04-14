25ce9566d84dfdbf9f093bb58c2c533073055e24
Sabya7 <sabya62.ss@gmail.com>
2024-04-14 23:00:00 +0530

    Reactor-POC | Sabya | Day 23 - Part10ReactiveToBlocking

---
+++ src/main/java/com/sabya/javapoc/reactive/learn/Part10ReactiveToBlocking.java
@@ -0,0 +1,23 @@
+package com.sabya.javapoc.reactive.learn;
+
+import com.sabya.javapoc.reactive.learn.model.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class Part10ReactiveToBlocking {
+
+//========================================================================================
+
+    // TODO Return the user contained in that Mono
+    User monoToValue(Mono<User> mono) {
+        return null;
+    }
+
+//========================================================================================
+
+    // TODO Return the users contained in that Flux
+    Iterable<User> fluxToValues(Flux<User> flux) {
+        return null;
+    }
+
+}
