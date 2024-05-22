3ce1c22d81901f4d94a7baca9c980d7e4f2a84b2
Sabya7 <sabya62.ss@gmail.com>
2024-05-22 23:48:33 +0530

    Reactor-POC | Sabya | Day 57 - POC

--- src/main/java/com/sabya/javapoc/jia/POC.java
+++ src/main/java/com/sabya/javapoc/jia/POC.java
@@ -5,6 +5,8 @@ import org.mockito.stubbing.Answer;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
@@ -195,6 +197,14 @@ class MeaningOfThis {
              if(dish.getCalories() < 400) {
                  lowCaloricDishes.add(dish);
              } }
+
+         Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
+             public int compare(Dish dish1, Dish dish2) {
+                 return Integer.compare(dish1.getCalories(), dish2.getCalories());
+             }
+         });
+
      }
+
 
  }
