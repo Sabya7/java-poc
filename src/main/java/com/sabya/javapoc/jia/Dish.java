cfca58d894d5ebdc7f4904aaa8a6d564cfa05241
Sabya7 <sabya62.ss@gmail.com>
2024-05-26 23:58:54 +0530

    Reactor-POC | Sabya | Day 61 - POC

--- src/main/java/com/sabya/javapoc/jia/Dish.java
+++ src/main/java/com/sabya/javapoc/jia/Dish.java
@@ -1,6 +1,14 @@
 package com.sabya.javapoc.jia;
 
 public class Dish {
+    public Dish(String pork, boolean b, int i, Object meat) {
+
+    }
+
+    public Dish() {
+
+    }
+
     public int getCalories() {
         return 5;
     }
@@ -18,5 +26,8 @@ public class Dish {
     }
 
     public class Type {
+        public static final Object MEAT = "meat";
+        public static final Object OTHER = "other";
+        public static final Object FISH = "fish";
     }
 }
