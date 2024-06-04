e27e438d101a30e59e8f383ba9c08cec6ec90717
Sabya7 <sabya62.ss@gmail.com>
2024-06-05 00:00:32 +0530

    Reactor-POC | Sabya | Day 69 - POC

--- src/main/java/com/sabya/javapoc/jia/POC.java
+++ src/main/java/com/sabya/javapoc/jia/POC.java
@@ -273,6 +273,11 @@ class MeaningOfThis {
                          .filter(Dish::isVegetarian)
                          .collect(toList());
 
+         List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
+         numbers.stream()
+                 .filter(i -> i % 2 == 0)
+                 .distinct()
+                 .forEach(System.out::println);
 
 
      }
