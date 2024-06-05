2efc2b7bc44a72e096a7a8bb75ecf752533dcaad
Sabya7 <sabya62.ss@gmail.com>
2024-06-05 23:37:32 +0530

    Reactor-POC | Sabya | Day 70 - POC

--- src/main/java/com/sabya/javapoc/jia/POC.java
+++ src/main/java/com/sabya/javapoc/jia/POC.java
@@ -279,6 +279,13 @@ class MeaningOfThis {
                  .distinct()
                  .forEach(System.out::println);
 
+         List<Dish> specialMenu = Arrays.asList(
+                 new Dish("seasonal fruit", true, 120, Dish.Type.OTHER),
+                 new Dish("prawns", false, 300, Dish.Type.FISH),
+                 new Dish("rice", true, 350, Dish.Type.OTHER),
+                 new Dish("chicken", false, 400, Dish.Type.MEAT),
+                 new Dish("french fries", true, 530, Dish.Type.OTHER));
+
 
      }
 
