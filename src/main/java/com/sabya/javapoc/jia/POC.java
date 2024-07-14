e42494dc467f29430b6713410e5ef6cf79a87460
Sabya7 <sabya62.ss@gmail.com>
2024-07-14 23:46:10 +0530

    Reactor-POC | Sabya | Day 108 - POC

--- src/main/java/com/sabya/javapoc/jia/POC.java
+++ src/main/java/com/sabya/javapoc/jia/POC.java
@@ -411,6 +411,11 @@ class MeaningOfThis {
                  .limit(10)
                  .forEach(System.out::println);
 
+         Stream.iterate(new int[]{0, 1},
+                         t -> new int[]{t[1], t[0]+t[1]})
+                 .limit(20)
+                 .forEach(t -> System.out.println("(" + t[0] + "," + t[1] +")"));
+
 
      }
 
