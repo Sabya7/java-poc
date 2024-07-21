2e1cbf32f41a2fed855fc0da44ed349cbd9c0c83
Sabya7 <sabya62.ss@gmail.com>
2024-07-21 22:26:44 +0530

    Reactor-POC | Sabya | Day 105 - POC

--- src/main/java/com/sabya/javapoc/jia/POC.java
+++ src/main/java/com/sabya/javapoc/jia/POC.java
@@ -442,6 +442,18 @@ class MeaningOfThis {
                  return 2; }
          });
 
+         IntSupplier fib = new IntSupplier(){
+             private int previous = 0;
+             private int current = 1;
+             public int getAsInt(){
+                 int oldPrevious = this.previous;
+                 int nextValue = this.previous + this.current;
+                 this.previous = this.current;
+                 this.current = nextValue;
+                 return oldPrevious;
+             } };
+         IntStream.generate(fib).limit(10).forEach(System.out::println);
+
 
      }
 
